99a8838b6e974c440e045c5b7d5ae89a9653947c
NaveedGujral <naveedgujral93@gmail.com>
2023-12-13 12:13:09 +0000

    removed testing code from getArticles

--- src/utils/getArticles.js
+++ src/utils/getArticles.js
@@ -3,9 +3,6 @@ import axios from "axios"
 const getArticles = () => {
     const URLpath = "https://ng-news.onrender.com/api/articles"
     
-    // tests loading screen DO NOT USE 
-    // const URLpath = "https://ng-news.onrender.com/api/article"
-    
     return axios
         .get(URLpath)
         .then((response) => {
