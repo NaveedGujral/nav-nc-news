6ce5c44e2e80d34a326a97923a486f596cadb340
NaveedGujral <naveedgujral93@gmail.com>
2024-03-23 21:34:08 +0000

    responsive styling applied to article list

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,35 +1,39 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const ArticleCard = (props) => {
-    const { articleObj } = props
-    const { article_img_url, article_id, title, author, created_at, votes, comment_count } =  articleObj 
+  const { articleObj } = props;
+  const {
+    article_img_url,
+    article_id,
+    title,
+    author,
+    created_at,
+    votes,
+    comment_count,
+  } = articleObj;
 
-    return (
-        <div className="ArticleCard">
-            <Link className="ArtLink" to={`/api/articles/${article_id}`}>
-            <img src={article_img_url} alt={`${title}`}/>
-                <div className="CardBanner">
-                        <h2>
-                            {title}
-                        </h2>
-                        <div className='ArticleCardMeta'>
-                            <h3>
-                                {author}
-                            </h3>
-                            <h3>
-                            {created_at.substring(8, 10)}/{created_at.substring(5, 7)}/{created_at.substring(0, 4)}
-                            </h3>
-                            <h3>
-                                {comment_count} Comments
-                            </h3>
-                            <h3>
-                                {votes} Votes
-                            </h3>
-                    </div> 
-                </div>
-            </Link>
+  return (
+    <div className="ArticleCard">
+      <Link className="ArtLink" to={`/api/articles/${article_id}`}>
+        
+          <img src={article_img_url} alt={`${title}`} />
+
+        <div className="CardBanner">
+          <h2>{title}</h2>
+          <div className="ArticleCardMeta">
+            <h3>{author}</h3>
+            <h3>
+              {created_at.substring(8, 10)}/{created_at.substring(5, 7)}/
+              {created_at.substring(0, 4)}
+            </h3>
+            <h3>{comment_count} Comments</h3>
+            <h3>{votes} Votes</h3>
+          </div>
         </div>
-    )
-}
 
-export default ArticleCard
+      </Link>
+    </div>
+  );
+};
+
+export default ArticleCard;
--- src/components/ListAllArticles.jsx
+++ src/components/ListAllArticles.jsx
@@ -1,55 +1,51 @@
-import ArticleCard from './ArticleCard';
-import { useEffect, useState } from 'react';
-import getArticles from '../utils/getArticles';
+import ArticleCard from "./ArticleCard";
+import { useEffect, useState } from "react";
+import getArticles from "../utils/getArticles";
 
 const ListAllArticles = () => {
-    const [articles, setArticles] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
-    const [isError, setIsError] = useState("")
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState("");
 
-    useEffect(() => {
-        getArticles()
-        .then((articles) => {
-            setArticles(articles)
-            setIsLoading(false)
-        })
-        .catch((err) => {
-            setIsLoading(false)
-            setIsError("could not retrieve articles")
-        })
-    }, [])
+  useEffect(() => {
+    getArticles()
+      .then((articles) => {
+        setArticles(articles);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setIsError("could not retrieve articles");
+      });
+  }, []);
 
-    if (isLoading) {
-        return (
-        <div className='LoadingScreen'>
-            <h2>
-                loading articles...
-            </h2>
+  if (isLoading) {
+    return (
+      <div className="LoadingScreen">
+        <h2>loading articles...</h2>
+      </div>
+    );
+  } else if (isError) {
+    return (
+      <div className="LoadingScreen">
+        <h2>{isError}</h2>
+      </div>
+    );
+  } else {
+    return (
+    <div className="ArticleList">
+        <div class="row">
+               { articles.map((article) => {
+                       return <ArticleCard articleObj={article} key={article.article_id}/>
+                   })}
         </div>
-        )
-    }
-    
-    else if (isError) {
-        return (
-        <div className='LoadingScreen'>
-            <h2>
-                {isError}
-            </h2>
-        </div>
-        ) 
-    }
-    
-    else {
-        return (
-        <ul className="ArticleList">
+      </div>
+    );
+  }
 
-            { articles.map((article) => {
-                    return <ArticleCard articleObj={article} key={article.article_id}/>
-                })}
-            
-        </ul>
-        )
-    }
-}
 
-export default ListAllArticles
+  
+};
+
+export default ListAllArticles;
+
